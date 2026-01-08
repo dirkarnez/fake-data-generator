5b7230b5831403197969584760e9ad70c647f22a
Dirk Arnez <smalldirkalex@gmail.com>
2026-01-08 16:01:46 +0800

    - update excel-worker.js

--- excel-worker.js
+++ excel-worker.js
@@ -4,23 +4,20 @@ importScripts("https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js
 // const XLSX = globalThis.XLSX;
 // debugger;
 self.onmessage = function(e) {
-  postMessage({});
-}
-
-/*
-
-  if (e.data.t === "process") {
+  if (e.data.type === "export") {
     // Use XLSX methods in the worker
-    const data = [
-      { name: "John", age: 30 },
-      { name: "Jane", age: 25 }
-    ];
+    const data = e.data.value;
+    // const data = [
+    //   { name: "John", age: 30 },
+    //   { name: "Jane", age: 25 }
+    // ];
     const ws = XLSX.utils.json_to_sheet(data);
     const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, "Data");
+    XLSX.utils.book_append_sheet(wb, ws, "users");
 
     // Generate the file data as a Uint8Array and send it back to the main thread
     const u8 = XLSX.write(wb, { type: "array", bookType: "xlsx" });
-    postMessage({t: "export", v: u8});
+    postMessage({type: "export", value: u8});
   }
-  */
+}
+
