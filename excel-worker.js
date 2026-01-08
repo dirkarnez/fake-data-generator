4a868cf46dd258e6b27db3b0e178c8e3223ea15b
Dirk Arnez <smalldirkalex@gmail.com>
2026-01-08 15:40:48 +0800

    - add excel-worker.js

---
+++ excel-worker.js
@@ -0,0 +1,5 @@
+importScripts("https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/shim.min.js");
+importScripts("https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/xlsx.full.min.js");
+
+const XLSX = globalThis.XLSX;
+debugger;
